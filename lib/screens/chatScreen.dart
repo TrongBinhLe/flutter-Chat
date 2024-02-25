694443e00ddee5c61518fad9de208527eb801e11
TrongBinhLe <ltbinhbk2010@gmail.com>
2024-02-25 08:45:13 +0700

    Adding chatMessage and input widget

--- lib/screens/chatScreen.dart
+++ lib/screens/chatScreen.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_chat/widgets/chat_messages.dart';
+import 'package:flutter_app_chat/widgets/new_message.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -8,21 +10,25 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('FlutterChat'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            icon: Icon(Icons.exit_to_app),
-            color: Theme.of(context).colorScheme.primary,
-          ),
-        ],
-      ),
-      body: Center(
-        child: Text('Loggin!'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('FlutterChat'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              icon: Icon(Icons.exit_to_app),
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ],
+        ),
+        body: Column(
+          children: const [
+            Expanded(
+              child: ChatMessages(),
+            ),
+            NewMessage(),
+          ],
+        ));
   }
 }
---
+++ lib/widgets/chat_messages.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class ChatMessages extends StatelessWidget {
+  const ChatMessages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text('No messages found'),
+    );
+  }
+}
