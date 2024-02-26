ca605d76a0baa7d7080bfa5bebf39d96f687052b
TrongBinhLe <ltbinhbk2010@gmail.com>
2024-02-26 21:05:10 +0700

    Adding Dark Mode for the app

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,30 @@ void main() async {
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 
+var kDartColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'FlutterChat',
+        darkTheme: ThemeData.dark().copyWith(
+          useMaterial3: true,
+          colorScheme: kDartColorScheme,
+          cardTheme: const CardTheme().copyWith(
+            color: kDartColorScheme.secondary,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kDartColorScheme.primaryContainer,
+              foregroundColor: kDartColorScheme.onPrimaryContainer,
+            ),
+          ),
+        ),
         theme: ThemeData(
           useMaterial3: true,
           // primarySwatch: Colors.yellow,
@@ -41,6 +60,7 @@ class MyApp extends StatelessWidget {
           elevatedButtonTheme: ElevatedButtonThemeData(
             style: ElevatedButton.styleFrom(
               backgroundColor: kColorScheme.primaryContainer,
+              foregroundColor: kColorScheme.onPrimaryContainer,
             ),
           ),
           textTheme: ThemeData().textTheme.copyWith(
@@ -50,6 +70,7 @@ class MyApp extends StatelessWidget {
                     fontSize: 14),
               ),
         ),
+        themeMode: ThemeMode.system,
         home: StreamBuilder(
           stream: FirebaseAuth.instance.authStateChanges(),
           builder: ((context, snapshot) {
